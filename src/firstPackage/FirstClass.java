080fb9b67527da942c6079dd263a40e85ea7eefa
Gokhan <gokhancili8607@gmail.com>
2022-05-22 19:11:18 +0200

    3. commit from local repository

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,8 +4,8 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("First commit from local repository");
-		System.out.println("Second commet from local repository");
-		
+		System.out.println("Second commit from local repository");
+		System.out.println("third commit from local repository");
 	}
 
 }
