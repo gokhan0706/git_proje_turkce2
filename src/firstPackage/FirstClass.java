14d19d43007fb4002e611e479d6b86a539bda732
gokhan0706 <57571309+gokhan0706@users.noreply.github.com>
2022-05-22 19:28:38 +0200

    conflit

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("First commit from local repository");
 		System.out.println("Second commit from local repository");
-		System.out.println("ucuncu commitimiz local repositoryde");
+		System.out.println("cnflit yapmak istiyorum");
 		System.out.println("First commit from remote repository");
 	}
 
