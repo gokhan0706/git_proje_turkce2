60ffa6ea8926dd6b4d3926fbfd38b0c37b31abea
gokhan0706 <57571309+gokhan0706@users.noreply.github.com>
2022-05-22 19:19:03 +0200

    Update FirstClass.java
    First commit from remote repository

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 		System.out.println("First commit from local repository");
 		System.out.println("Second commit from local repository");
 		System.out.println("third commit from local repository");
+		System.out.println("First commit from remote repository");
 	}
 
 }
